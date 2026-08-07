cda285d698c5b4af573e0dcb5db28705eabce963
N21Voldigoad <185027455+N21Voldigoad@users.noreply.github.com>
2026-08-07 22:02:36 +0700

    Add files via upload

---
+++ assets/js/core/app.js
@@ -0,0 +1,20 @@
+const loadingScreen = document.getElementById("loading-screen");
+const verificationScreen = document.getElementById("verification-screen");
+
+const progressBar = document.getElementById("progress-bar");
+const progressText = document.getElementById("progress-text");
+const subtitle = document.getElementById("loading-subtitle");
+
+const subtitles = [
+
+    "Sedang menyiapkan hadiah...",
+
+    "Hadiah masih dihias...",
+
+    "Bersiap menerima hadiahnya...",
+
+    "Hampir siap...",
+
+    "Telah siap"
+
+];
